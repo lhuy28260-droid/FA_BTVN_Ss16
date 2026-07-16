09e08d5cf249be0a160c4da818b4a06125aba689
Luong Huy <email_cua_huy@gmail.com>
2026-07-16 07:33:26 +0700

    FA_BTVN_Ss18

--- Sangtao1/app/models/workshop.py
+++ Sangtao1/app/models/workshop.py
@@ -1,39 +1,40 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+# app/models/workshop.py
+from sqlalchemy import Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from datetime import datetime
-from database import Base
+from app.database import Base
 
 class Registration(Base):
-    """
-    Model Registration: Bảng trung gian liên kết (N-N)
-    Chứa các ràng buộc khóa ngoại chặt chẽ cấp độ Database để tránh dữ liệu rác.
-    """
     __tablename__ = "registrations"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     student_id = Column(Integer, ForeignKey("students.id", ondelete="CASCADE"), nullable=False)
     workshop_id = Column(Integer, ForeignKey("workshops.id", ondelete="CASCADE"), nullable=False)
     registered_at = Column(DateTime, default=datetime.utcnow, nullable=False)
+    status = Column(String(30), default="SUCCESS", nullable=False) # SUCCESS, CANCELLED
 
 
 class Student(Base):
-    """Model Student: Thực thể Sinh viên"""
     __tablename__ = "students"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    student_code = Column(String(20), unique=True, nullable=False, index=True)
+    student_code = Column(String(50), unique=True, nullable=False, index=True)
     full_name = Column(String(100), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
+    status = Column(String(30), default="ACTIVE", nullable=False) # ACTIVE, INACTIVE
 
+    # Thiết lập mối quan hệ Nhiều-Nhiều thông qua bảng trung gian registrations
     workshops = relationship("Workshop", secondary="registrations", back_populates="students")
 
 
 class Workshop(Base):
-    """Model Workshop: Thực thể Buổi hội thảo ngắn hạn"""
     __tablename__ = "workshops"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
     title = Column(String(150), nullable=False)
+    description = Column(String(255), nullable=True)
     maximum_participants = Column(Integer, nullable=False)
+    status = Column(String(30), default="OPENNING", nullable=False) # OPENNING, CLOSED, CANCELLED
+    start_time = Column(DateTime, nullable=False)
 
     students = relationship("Student", secondary="registrations", back_populates="workshops")
